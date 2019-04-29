f626d20a4e2fdb6f64134817b170eee72d85d5b6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-29 14:38:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,18 @@
 # Your code goes here!
 class Anagram
 
-    def match
+  @@all = []
+  def initialize(word)
+      @all 
+      
+  end
 
+    def match(word_array)
+      word_array.each do |elem|
+          if elem.split("").sort = word.split("").sort
+              @all << elem
+          end
+      end
     end
 
 end
